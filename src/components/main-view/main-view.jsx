94e22eefd3d55808cab83d870a33da2b9056c292
Hermann Rasch <hermann.rasch93@gmail.com>
2023-01-22 23:39:12 +0100

    addition of a new package, and 2 components, and updates to the main-view
    Addition of prop Types.
    Addition of movie-card & movie-view.
    Update to the main-view file ()

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
-import { MovieCard } from "../main-view/movie-card";
-import { MovieView } from "../main-view/movie-view"; 
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view"; 
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-{ id: 1, title: "The Lord of The Rings: The Fellowship of the Ring",
-image:"https://www.imdb.com/title/tt0120737/mediaviewer/rm3360781312/?ref_=tt…"},
-{ id: 2, title: "The Lord of The Rings: The Two Towers",
-image:"https://www.imdb.com/title/tt0167261/mediaviewer/rm2518119424/?ref_=tt…"},
-{ id: 3, title: "The Lord of The Rings: The Return of The King",
-image:"https://www.imdb.com/title/tt0167260/mediaviewer/rm218277121/?ref_=tt_…"},
-{ id: 4, title: "Star Wars: Episode I- The Phantom Menace",
-image:"https://www.imdb.com/title/tt0120915/mediaviewer/rm3410261504/?ref_=tt…"},
-{ id: 5, title: "Star Wars: Episode V- The Empire Strikes Back",
-image:"https://www.imdb.com/title/tt0080684/mediaviewer/rm3228333312/?ref_=tt…"}
-    ]);
+    const [movies, setMovies] = useState([]);
+// { id: 1, title: "The Lord of The Rings: The Fellowship of the Ring",
+// image:"https://www.imdb.com/title/tt0120737/mediaviewer/rm3360781312/?ref_=tt_ov_i"},
+// { id: 2, title: "The Lord of The Rings: The Two Towers",
+// image:"https://www.imdb.com/title/tt0167261/mediaviewer/rm2518119424/?ref_=tt_ov_i"},
+// { id: 3, title: "The Lord of The Rings: The Return of The King",
+// image:"https://www.imdb.com/title/tt0167260/mediaviewer/rm218277121/?ref_=tt_ov_i"},
+// { id: 4, title: "Star Wars: Episode I- The Phantom Menace",
+// image:"https://www.imdb.com/title/tt0120915/mediaviewer/rm3410261504/?ref_=tt_ov_i"},
+// { id: 5, title: "Star Wars: Episode V- The Empire Strikes Back",
+// image:"https://www.imdb.com/title/tt0080684/mediaviewer/rm3228333312/?ref_=tt_ov_i"}
+useEffect(() => {
+    fetch("")
+    .then((response) => response.json())
+    .then((data) => {
+        const moviesFromApi = data.docs.maps((doc) => {
+            return {
+                id: doc.key,
+                title: doc.title,
+                image: ``,
+                director: 
+            };
+        });
+        setMovies(moviesFromApi); 
+    });
+}, []);
     // set default state to null(default-state)
     const [selectedMovie, setSelectedMovie] = useState(null);
     if (selectedMovie) {
