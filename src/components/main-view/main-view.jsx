c1da5d0135566052c4424433278ecfbd08684d01
Hermann <91563884+HerRA17@users.noreply.github.com>
2023-02-11 17:19:58 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,16 +4,7 @@ import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
-// { id: 1, title: "The Lord of The Rings: The Fellowship of the Ring",
-// image:"https://www.imdb.com/title/tt0120737/mediaviewer/rm3360781312/?ref_=tt_ov_i"},
-// { id: 2, title: "The Lord of The Rings: The Two Towers",
-// image:"https://www.imdb.com/title/tt0167261/mediaviewer/rm2518119424/?ref_=tt_ov_i"},
-// { id: 3, title: "The Lord of The Rings: The Return of The King",
-// image:"https://www.imdb.com/title/tt0167260/mediaviewer/rm218277121/?ref_=tt_ov_i"},
-// { id: 4, title: "Star Wars: Episode I- The Phantom Menace",
-// image:"https://www.imdb.com/title/tt0120915/mediaviewer/rm3410261504/?ref_=tt_ov_i"},
-// { id: 5, title: "Star Wars: Episode V- The Empire Strikes Back",
-// image:"https://www.imdb.com/title/tt0080684/mediaviewer/rm3228333312/?ref_=tt_ov_i"}
+
 useEffect(() => {
     fetch("https://movies-couch-api-git-main-herra17.vercel.app/")
     .then((response) => response.json())
