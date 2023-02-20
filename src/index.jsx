2932181f556c0bde05da8d4f8e2c0856b44daa31
Hermann Rasch <hermann.rasch93@gmail.com>
2023-02-20 19:22:09 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,11 @@ import { MainView } from './components/main-view/main-view';
 // Import statement to indicating bundle is needed 
 import "./index.scss";
 // Main component rendered
-const MoviescouchApplication = () => {
+const MoviesCouchApplication = () => {
     return <MainView />;
 };
 // Find the root of your app
 const container  = document.querySelector("#root");
 const root = createRoot(container);
 // Tells React to render your app in the root DOM element
-root.render(<MoviescouchApplication/>);
+root.render(<MoviesCouchApplication/>);
