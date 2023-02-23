622685f3e7006473d10a7b499b855e8f26b299db
Hermann Rasch <hermann.rasch93@gmail.com>
2023-02-24 00:51:09 +0100

    Update signup-view.jsx

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,69 +1,69 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// export const SignupView = () => {
-//     const [username, setUsername] = useState(""); 
-//     const [password, setPassword] = useState("");
-//     const [email, setEmail] = useState("");
-//     const [birthday, setBirthday] = useState("");
+export const SignupView = () => {
+    const [username, setUsername] = useState(""); 
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthday, setBirthday] = useState("");
 
 // validation of signup view
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//     const data = {
-//         Username: username,
-//         Password: password,
-//         Email: email,
-//         Birthday: birthday
-//         };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    const data = {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+        };
 
-//     fetch("https://movies-couch-api.vercel.app/users", {
-//         method: "POST",
-//          body: JSON.stringify(data),         
-//          headers: {
-//             "Content-Type": "application/json"
-//         }                 
-//         }).then((response) => {
-//          if (response.ok) {
-//             alert("Signup successful");
-//              window.location.reload();
-//              } else {
-//                  alert("Signup failed")
-//                 } 
-//             });
-//          };
+    fetch("https://movies-couch-api.vercel.app/users", {
+        method: "POST",
+         body: JSON.stringify(data),         
+         headers: {
+            "Content-Type": "application/json"
+        }                 
+        }).then((response) => {
+         if (response.ok) {
+            alert("Signup successful");
+             window.location.reload();
+             } else {
+                 alert("Signup failed")
+                } 
+            });
+         };
 // signup form with submit button
-//         return (
+        return (
 // handleSubmit is the callback of onSubmit, tells the login API to validate user & password
-//         <form onSubmit={handleSubmit}>
-//             <label>
-//                 Username:
-//                 <input type= "text" 
-//                 value={username}
-//                 onChange={(e) => setUsername(e.target.value)}
-//                 minLength="5"/>
-//             </label>
-//             <label> 
-//                 Password:
-//                 <input type="password"
-//                  value={password}
-//                  onChange={(e) => setPassword(e.target.value)}
-//                  required />
-//             </label>
-//             <label>
-//                 Email:
-//                 <input type="email"
-//                 value={email}
-//                 onChange={(e) => setEmail(e.target.value)}
-//                 required />
-//             </label>
-//             <label>
-//                 Birthday:
-//                 <input type="date"
-//                 value={birthday}
-//                 onChange={(e) => setBirthday(e.target.value)}
-//                 required />
-//             </label>
-//             <button type="submit">Submit</button>
-//         </form>
-//     );
-// };
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type= "text" 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                minLength="5"/>
+            </label>
+            <label> 
+                Password:
+                <input type="password"
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                 required />
+            </label>
+            <label>
+                Email:
+                <input type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required />
+            </label>
+            <label>
+                Birthday:
+                <input type="date"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                required />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
