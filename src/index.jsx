182119c94be9ba9457b3eef19a7085bd3e2b5ff2
Hermann Rasch <hermann.rasch93@gmail.com>
2023-02-28 13:35:17 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot} from "react-dom/client";
 // import Main-component
-import {MainView} from "../src/components/main-view/main-view" ;  //"../src/components/main-view/main-view";  or './tests/test.main-view'
+import {MainView} from "../src/components/main-view/main-view";  
 // Import statement to indicating bundle is needed 
 import "./index.scss";
 // Main component rendered
