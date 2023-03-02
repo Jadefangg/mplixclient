1f0a9af38f26c408ea90021a798bc23f8a927209
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-03 00:00:54 +0100

    addition of some components

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { useState } from "react";
+// importing Container
+import Container from "react-bootstrap/Container";
+// importing Card
+import Card from "react-bootstrap/Card";
+// importing Columns
+import Col from "react-bootstrap/Col";
+// importing Row
+import Row from "react-bootstrap/Row";
 // import Button feature 
 import Form from "react-bootstrap/Form";
 // importing Button feature 
 import Button from "react-bootstrap/Button";
+
 
 export const LoginView = ({ onLoggedIn }) => {
  const [username, setUsername] = useState("");
@@ -41,28 +50,41 @@ export const LoginView = ({ onLoggedIn }) => {
 //  login form with submit button
     return (
         // handle submit is the callback of onSubmit, tells the login API to validate user & password
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formUsername">
-            <Form.Label>Username:</Form.Label>
-            <Form.Control 
-            type="text"
-            value={username}
-            onChange= {(e) => setUsername(e.target.value)}
-            minLength="3"
-            required
-            />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-            <Form.Label></Form.Label>
-            <Form.Control 
-            type="password"
-            value={password}
-            onChange= {(e) => setPassword(e.target.value)}
-            minLength="5" 
-            required
-            />
-            </Form.Group>
-            <Button variant="primary" type="submit" /*className="" - looking for a way to separte the button from the password rectangle*/>Submit</Button>  
-        </Form>
+        <Container>
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card>
+                            <Card.Body>
+                            <Card.Title>Pleas Login</Card.Title>
+                            <Form className="movies-couch" onSubmit={handleSubmit}>
+                                <Form.Group controlId="formUsername">
+                                <Form.Label>Username:</Form.Label>
+                                <Form.Control 
+                                type="text"
+                                value={username}
+                                onChange= {(e) => setUsername(e.target.value)}
+                                minLength="3"
+                                required
+                                />
+                                </Form.Group>
+                                <Form.Group controlId="formPassword">
+                                <Form.Label>Password:</Form.Label>
+                                <Form.Control 
+                                type="password"
+                                value={password}
+                                onChange= {(e) => setPassword(e.target.value)}
+                                minLength="5" 
+                                required
+                                />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">Submit</Button>  
+                            </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
     );
 };
