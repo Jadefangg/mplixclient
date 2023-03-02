ed309d7a19e2f09bf664e2a997925271491d39d2
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-03 00:02:49 +0100

    addition of bootstrap components

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 // importing from r-bootstrap Form feature
 import Form from"react-bootstrap/Form";
+// importing Container
+import Container from "react-bootstrap/Container";
+// importing Card
+import Card from "react-bootstrap/Card";
 // importing from r-bootstrap Button feature
 import Button from "react-bootstrap/Button";
 
@@ -38,41 +42,51 @@ export const SignupView = () => {
 // signup form with submit button
         return (
 // handleSubmit is the callback of onSubmit, tells the login API to validate user & password
-    <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formUsername">
-                    <Form.Label>Username:</Form.Label>
-                <Form.Control type="text" 
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                minLength="3"
-                />
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required 
-            minLength="5" />
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-            <Form.Label>Email:</Form.Label>
-            <Form.Control type="text" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required/>
-        </Form.Group>
-        <Form.Group controlId="Birthdate">
-            <Form.Label>Birthdate:</Form.Label>
-            <Form.Control type="date"
-            value={birthdate}
-            onChange={(e) => setBirthdate(e.target.value)}
-            required
-            />
-        </Form.Group>
-                <Button variant="primary" type="submit">Submit</Button> 
-    </Form> 
+<Container>
+    <Row>
+        <Col>
+            <Card>
+                <Card.Body>
+                    <Form className="movies-couch" onSubmit={handleSubmit}>
+                                <Form.Group controlId="formUsername">
+                                    <Form.Label>Username:</Form.Label>
+                                <Form.Control type="text" 
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                                minLength="3"
+                                />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required 
+                            minLength="5" />
+                        </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control type="text" 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required/>
+                        </Form.Group>
+                        <Form.Group controlId="Birthdate">
+                            <Form.Label>Birthdate:</Form.Label>
+                            <Form.Control type="date"
+                            value={birthdate}
+                            onChange={(e) => setBirthdate(e.target.value)}
+                            required
+                            />
+                        </Form.Group>
+                                <Button variant="primary" type="submit">Submit</Button> 
+                    </Form> 
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+</Container>
 
     );
 };
