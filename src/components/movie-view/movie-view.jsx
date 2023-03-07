aab0036192c4945674224ee60c5eeaf3e4f223a5
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-07 21:11:38 +0100

    Removal of comments

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,4 @@
-
-// importing Card component
 import { Button, Card } from "react-bootstrap";
-// Params
 import { useParams } from "react-router";
 import Link from "react-router-dom";
 import "./movie-view.scss";
@@ -10,26 +7,25 @@ export const MovieView = ({ movies }) => {
     const {movieId} = useParams();
     const movie = movies.find((m) => m._id === movieId);
 return( 
-<Card className="movie-view">
-    <Card.Img className="w-80" src={movie.ImageURL} />
-    <Card.Body className="movie-view">
-        <Card.Title>{movie.Title}</Card.Title>
-        <br />
-        <Card.Text> Director:      
-         {' ' + movie.Director.Name} 
-        <br /> <br />
-        About: {' ' +movie.Director.Bio}
-        <br /> <br />Birthdate: 
-        {' ' + movie.Director.Birthdate}
-        <br /> <br />Genre: 
-        {' ' + movie.Genre.Name}
-        </Card.Text>
-        <br />
-        <Link to={"/"}>
-        <Button className="back-button" style={{ cursor: "pointer"}} active>Back</Button>
-        </Link>
-    </Card.Body>
-</Card> 
-
+    <Card className="movie-view">
+        <Card.Img className="w-80" src={movie.ImageURL} />
+        <Card.Body className="movie-view">
+            <Card.Title>{movie.Title}</Card.Title>
+            <br />
+            <Card.Text> Director:      
+             {' ' + movie.Director.Name} 
+            <br /> <br />
+            About: {' ' +movie.Director.Bio}
+            <br /> <br />Birthdate: 
+            {' ' + movie.Director.Birthdate}
+            <br /> <br />Genre: 
+            {' ' + movie.Genre.Name}
+            </Card.Text>
+            <br />
+            <Link to={"/"}>
+            <Button className="back-button" style={{ cursor: "pointer"}} active>Back</Button>
+            </Link>
+        </Card.Body>
+    </Card> 
     );
 };
