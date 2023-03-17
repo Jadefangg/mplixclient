232123b459a161c6ee247cf5a7c3daab3e030bc9
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-17 12:34:38 +0100

    Testing

--- src/components/profile-view/test-profile.jsx
+++ src/components/profile-view/test-profile.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import Form from "react-bootstrap/Form";
 import { Col, Row}  from "react-bootstrap";
 import { Button, Card, Container } from "react-bootstrap";
+import axios from "axios";
 
 
 // import UserInfo from "./user-info"; 
@@ -9,150 +10,40 @@ import { Button, Card, Container } from "react-bootstrap";
 // import FavoriteMovies from "./favorite-movies";
 
     export const TestProfile= ({user, movies, token}) => {
-           const [user, setUset] = useState({
-            username: "",
-            password: "",
-            email: "",
-            birthday: ""
-        });
-        setUser({
-            ...user,
-            username: "newUsername"
-          }); //could be integrated to edit function
-          
-        // const [user, setUser] = setState([]);
-   // user related functions
-    // useEffect(() => {
-    //     const user= JSON.parse(localStorage.getItem("user"));
-    //     if (user) {setUser(user);}
-
-        // if(!user && !token) {
-        //     return window.open("/", "_self")}
-    // }, [])
-    // get uSer
-    // function getUser(username) {
-        //   Make a request to retrieve user data from API
-        //   return fetch(`/users/${username}`)
-        //   .then(response => response.json())
-        //   .then(data => {
-            // Extract user information from the response and return it as an object
-        //     const { username, password, email, birthday } = data;
-        //     return { username, password, email, birthday };
-        //   })
-        //   .catch(error => {
-        //     console.error(`Error retrieving user: ${username}: `, error);
-        //   });
-        // }
-        // const [updateUser, setUpdateUser] = useState(false);
-            
-    // update user
-    function editUser() {
-        const [formData, setFormData] = useState({username:"", password: "", email: "", birthday:""}); 
-        const handleChange = (event) => {
-            const {username, value} = event.target;
-            setFormData((prevFormData) => ({...prevFormData, [username]:value}))
-        };
-        const handleSubmit =  (event) => {
-            event.preventDefault();
-            alert(`Username: ${formData.username}, Password: ${formData.password}, Email: ${formData.email}, Birthday: ${formData.birthday}`)
-        } 
-    };
+    //    update user dunction
+        function editUser({user, token})  {
+            const [formData, setFormData] = useState({
+                username: user.username,
+                password: user.password,
+                email: user.email,
+                birthday: user.birthday
+            })
+            const handleSubmit= event => {
+                event.preventDefault();
+                const token = localStorage.getItem("token");
+                axios.put(`https://movies-couch-api.vercel.app/users/${user.username}`,
+                formData,
+                {headers: {Authorization: `Bearer ${token}`}}
+                )
+                .then((response) =>{
+                    console.log(response.data);
+                    alert("Profile updated!");
+                    window.open(`/users/${user.username}`, "_self");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+            const handleChange = event => {
+                const name = event.target.name;
+                const value = event.target.value;
+                setFormData(prevState => ({...prevState, [name]: value})); 
+            }
+        }
+    
     
     // delete user
-    // const handleDeleteUser = async () => {
-    //     try {
-    //         const response = await fetch(
-    //             `https://movies-couch-api.vercel.app/users/${user.username}`,
-    //             {
-    //                 method: "DELETE",
-    //                 headers: {
-    //                     Authorization: `Bearer ${token}`,
-    //                     "Content-Type": "application/json",
-    //                 },
-    //             }
-    //         );
-    //         const { success, message, data } = await response.json();
-    //         if (success) {
-    //             onDelete();
-    //         } else {
-    //             alert(message);
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
-
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log("submitted");
-    // }
-
-    // favorite-movies functions
-    // const onRemoveFavorite = (_id) => {}
-    // const handleEdit = async (event) => {
-    //     event.preventDefault();
-    //     const userData={
-    //         username: username,
-    //         password: password,
-    //         email: email,
-    //         birthday: birthday
-    //     }; 
-        // console.log(userData)
-    //     try {
-    //         const response = await fetch(
-    //             `https://movies-couch-api.vercel.app/users/${user.username}`,
-    //             {
-    //                 method: "PUT",
-    //                 body: JSON.stringify(userData),
-    //                 headers: {
-    //                     Authorization: `Bearer ${token}`,
-    //                     "Content-Type": "application/json",
-    //                 },
-    //             }
-    //         );
-    //         const { success, message, data } = await response.json();
-    //         if (success) {
-    //             alert(message);
-    //             setUpdateUser(false);
-    //         } else {
-    //             console.error(message);
-    //             alert("Update failed");
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
-
-    // const favoriteMovies = movies.filter(m => {
-    //     user.favoriteMovies.includes(m._id);
-    // }); //fav mov only needs from there a button
-
-    // log-out function
-    // onLoggedOut = () =>  {
-    //     localStorage.removeItem("token");
-    //     localStorage.removeItem("user");
-    //     this.setState({ user:null });
-    //     window.open("/", "_self")
-    // } 
-    // useEffect(() => {
-    //     if (birthdayInputRef.current) {
-    //         birthdayInputRef.current.value = formatDate(birthday);
-    //     }
-    // }, [updateUser])
-
-    // const formatDate = (birthday) => {
-    //     const date = new Date(birthday);
-    //     const year = date.getFullYear();
-    //     let month = date.getMonth() + 1;
-    //     let dayOfTheMonth = date.getDate();
-    //     if (month < 10) {
-    //         month = `0${month}`;
-    //     }
-    //     if (dayOfTheMonth < 10) {
-    //         dayOfTheMonth = `0${dayOfTheMonth}`;
-    //     }
-    //     return `${year}-${month}-${dayOfTheMonth}`;
-    // };
+    // remove fav-mov
+    // 
 
     return (
         <React.Fragment>
@@ -176,11 +67,6 @@ import { Button, Card, Container } from "react-bootstrap";
                              <br/>
                             </>
                             </Card.Body>
-                            <Button className="edit-button"
-                                variant="primary"
-                                // onClick={() => setUpdateUser(true)}
-                                >EDIT
-                            </Button>
                         </Card>
                     </Col>
                     </Row> 
@@ -190,40 +76,35 @@ import { Button, Card, Container } from "react-bootstrap";
                                 <Card.Body>
                                     <Card.Title>Profile Information</Card.Title>
                                     <Card.Text></Card.Text>
-                                    <form onSubmit={editUser} className="w-100">
-                                        <label htmlFor="username"></label>
-                                        <input type="text" id="username" name="username" value={formData.username} onChange={handleChange}></input>
-                                        <br/>
-                                        <label htmlFor="password"></label>
-                                        <input type="password" id="password" name="password" value={formData.password} onChange={handleChange}></input>
-                                        <br/>
-                                        <label htmlFor="email"></label>
-                                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}></input>
-                                        <br/>
-                                        <label htmlFor="birthday"></label>
-                                        <input type="date" id="birthday" name="birthday" value={formData.birthday} onChange={handleChange}></input>
+                                        <form >
+                                        <label>
+                                            Username:
+                                            <input type="text" name="username" value={formData.username} onChange={handleChange} />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Password:
+                                            <input type="password" name="password" value={formData.password} onChange={handleChange} />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Email:
+                                            <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Birthday:
+                                            <input type="date" name="birthday" value={formData.birthday} onChange={handleChange} />
+                                        </label>
+                                        <br />
+                                        <button  onClick={editUser}>Update User</button>
                                     </form>
                                 </Card.Body>
                             </Card>
                         </Col>
                     </Row>
-
-                    {/* <Row className="justify-content-center py-5">
-                    <h2 className="text-center mb-5">Favorite Movies</h2>
-                    {favoriteMovies.length ? (
-                        favoriteMovies.map((movie) => (
-                            <MovieCard
-                                movie={movie}
-                                isFavorite={true}
-                                toggleFavorite={handleToggle}
-                                _id={movie._id}
-                            />
-                        ))
-                    ) : (
-                        <p>No favorite movies</p>
-                    )}
-                </Row> */}
-        </Container>
-    </React.Fragment>
-    );
+            </Container>
+        </React.Fragment>
+        );
+    };
 };
