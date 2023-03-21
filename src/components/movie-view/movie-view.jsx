f26cfc8108ff15efebf9e7fd5d958601508bb528
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-21 17:28:48 +0100

    Review process

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,14 +4,17 @@ import { useParams } from "react-router";
 import { Link, Redirect } from "react-router-dom";
 import "./movie-view.scss";
 
-export const MovieView = ({ movies }) => {
+export const MovieView = ({ movies, favoriteMovies }) => {
     const {movieId} = useParams();
     const movie = movies.find((m) => m._id === movieId);
     
     if (!movie) {
         // if movie is not found return to homepage
         return <Redirect to="/" />;
-    }
+    };
+    function addMovie(){
+        favoriteMovies.add();
+      }
 return( 
     <Card className="movie-view">
         <Card.Img className="w-80" src={movie.ImageURL} alt="movie-poster"/>
@@ -29,7 +32,8 @@ return(
             </Card.Text>
             <br />
             <Link to={"/"}>
-            <Button className="back-button" variant="button" style={{ cursor: "pointer"}} active>Back</Button>
+            <Button variant="button" className="back-button" style={{ cursor: "pointer"}} active>Back</Button>
+            <Button onClick={addMovie}>Add to Favorite List</Button>
             </Link>
         </Card.Body>
     </Card> 
