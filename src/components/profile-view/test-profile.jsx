3c9cfb4817e7ad7fa81e382f144b49899cdf1078
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-23 23:11:59 +0100

    Under review and testing

--- src/components/profile-view/test-profile.jsx
+++ src/components/profile-view/test-profile.jsx
@@ -1,18 +1,18 @@
 import  {React, useEffect, useState} from "react"; 
 // import Form from "react-bootstrap/Form";
-import { Col, Row}  from "react-bootstrap";
+import { Col, ListGroup, ListGroupItem, Row}  from "react-bootstrap";
 import { Card, Container } from "react-bootstrap";
 import axios from "axios";
 import FavoriteMovies from "./favorite-movies";
 
 
     export const TestProfile= ({user, setUser, movies, token}) => {
-        const [username, setUsername] = useState();
-        const [password, setPassword] = useState();
-        const [email, setEmail] = useState();
-        const [birthday, setBirthday] = useState();
+        const [username, setUsername] = useState("");
+        const [password, setPassword] = useState("");
+        const [email, setEmail] = useState("");
+        const [birthday, setBirthday] = useState("");
         //    update user function
-            const handleSubmit= event => {
+            const handleSubmit= (event) => {
                 event.preventDefault();
                 const token = localStorage.getItem("token");
                 const data = {
@@ -34,7 +34,14 @@ import FavoriteMovies from "./favorite-movies";
                 console.log(error);
             });
     };
-    //get users 
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        setUsername(e.target.value);
+        setPassword(e.target.value);
+        setEmail(e.target.value);
+        setBirthday(e.target.value);
+    }   
+     //get users 
     useEffect(() =>{
         const token = localStorage.getItem("token");
         axios.get(`https://movies-couch-api.vercel.app/users/${user.Username}`,
@@ -46,25 +53,27 @@ import FavoriteMovies from "./favorite-movies";
             console.log(error);
         });
     }, [user.Username, token]);
+    // delete user
     // Fav-movies 
     const favoriteMovies = movies.filter(m => user.FavoriteMovies.includes(m._id)) 
     // remove-fav_Movies
-     removeFavMovie = async (movies) =>{
-        const user = localStorage.getItem("user");
-        console.log(user);
-        const token = localStorage.getItem("token");
-        console.log(token);
-        axios.delete(`https://movies-couch-api.vercel.app/users/${user.Username}/movies/${movies._id}`,
-        {header: {Authorization: `Bearer ${token}`}}
-        ) 
-        .then((response) => {
-            console.log(response);
-            alert(`The Movie: ${movies._id} was removed from Favorite List`)
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-     }
+  removeFavMovie = async (movies) =>{
+    const user = localStorage.getItem("user");
+    console.log(user);
+    const token = localStorage.getItem("token");
+    console.log(token);
+    axios.delete(`https://movies-couch-api.vercel.app/users/${user.Username}/movies/${movies._id}`,
+    {header: {Authorization: `Bearer ${token}`}}
+    ) 
+    .then((response) => {
+        console.log(response);
+        alert(`The Movie: ${movies._id} was removed from Favorite List`)
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+ }
+
     return (
         <Container className="profile-view">
                 <Row className="d-flex justify-content-center p-4">
@@ -73,16 +82,16 @@ import FavoriteMovies from "./favorite-movies";
                             <Card.Body className="profile-view">
                             <>
                              <h3>Username:</h3>
-                             <p >{user.Username}</p>
+                             <p >{username}</p>
                              <br/>
-                             <h3>Password:</h3>
-                             <p >{user.Password}</p>
-                             <br/>
+                             {/* <h3>Password:</h3>
+                             <p >{setPassword}</p>
+                             <br/> */}
                              <h3>Email:</h3>
-                             <p >{user.Email}</p>
+                             <p >{email}</p>
                              <br/>
                              <h3 >Birthday:</h3>
-                             <p >{user.Birthday}</p>
+                             <p >{birthday}</p>
                              <br/>
                             </>
                             </Card.Body>
@@ -95,48 +104,65 @@ import FavoriteMovies from "./favorite-movies";
                                 <Card.Body className="form-profile">
                                     <Card.Title>Profile Information</Card.Title>
                                     <Card.Text></Card.Text>
+                                    <ListGroup>
                                         <form onSubmit={handleSubmit}>
-                                        <label>
-                                            Username:
-                                            <input 
-                                            type="text"
-                                            name="username"
-                                            // value={user.Username}
-                                            onChange={(e) => setUsername(e.target.value)}
-
-                                             /> 
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Password:
-                                            <input 
-                                            type="password"
-                                            name="password"
-                                            // value={user.Password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                             />
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Email:
-                                            <input 
-                                            type="email"
-                                            name="email"
-                                            // value={user.Email}
-                                            onChange={(e) => setEmail(e.target.value)}  />
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Birthday:
-                                            <input 
-                                            type="date" 
-                                            name="birthday"
-                                            // value={user.Birthday}
-                                            onChange={(e) => setBirthday(e.target.value)}  />
-                                        </label>
-                                        <br />
-                                        <button  type="submit" >Update User</button>
-                                    </form>
+                                            <ListGroup.Item>
+                                            <label>
+                                                Username:
+                                                <input 
+                                                type="text"
+                                                name="username"
+                                                class=".li-element"
+                                                // value={user.Username}
+                                                onChange={(e) => handleUpdate(e)}
+                                                /> 
+                                            </label>
+                                            </ListGroup.Item>
+                                            <br/>
+                                           <ListGroup.Item>   
+                                            <label>
+                                                Password:
+                                                <input 
+                                                type="password"
+                                                name="password"
+                                                class=".li-element"
+                                                // value={user.Password}
+                                                onChange={(e) => handleUpdate(e)}
+                                                 />
+                                            </label>
+                                            </ListGroup.Item> 
+                                            <br />
+                                            <ListGroup.Item>  
+                                            <label>
+                                                Email:
+                                                <input 
+                                                type="email"
+                                                name="email"
+                                                class=".li-element"
+                                                // value={user.Email}
+                                                onChange={(e) => handleUpdate(e)}  
+                                                />
+                                            </label>
+                                            </ListGroup.Item>   
+                                            <br />
+                                            <ListGroup.Item>
+                                            <label>
+                                                Birthday: 
+                                                <input 
+                                                type="date" 
+                                                name="birthday"
+                                                class=".li-element"
+                                                // value={user.Birthday}
+                                                onChange={(e) => handleUpdate(e)}
+                                                 />
+                                            </label>
+                                            </ListGroup.Item>
+                                            <br />
+                                            <ListGroup.Item>
+                                            <button  type="submit" class=".li-element">Update User</button>
+                                            </ListGroup.Item>
+                                        </form>
+                                    </ListGroup>
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -144,8 +170,18 @@ import FavoriteMovies from "./favorite-movies";
                     <Row>
                         <Col>
                         {/* <FavoriteMovies /> */}
+                        {/* <Button onClick={removeFavMovie} className="movie-card-button">Remove from Favorite List</Button> */}
                         </Col>
                     </Row>
             </Container>
         ); 
+};
+TestProfile.propTypes = {
+    user: Proptypes.shape({
+    _id: Proptypes.number.isRequired,
+    username: Proptypes.string.isRequired,
+    password: Proptypes.string.isRequired,
+    email: Proptypes.string.isRequired,
+    birthday: Proptypes.date.isRequired
+})
 };
