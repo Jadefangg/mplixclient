785b59e617bc997b614bd0cc394c95d32e2a655a
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-23 23:11:53 +0100

    Under review and testing

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -3,94 +3,48 @@ import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form"
 
 
-function UpdateUser({handleSubmit, handleUpdate}) {
+function UpdateUser({editUser, setUsername, setPassword, setEmail, setBirthday, onDeleteUser}) {
+
     return (
-        <Form onSubmit={(e) => handleSubmit(e)}> 
+        <Form onSubmit={(e) => this.editUser((e), this.Username, this.Password, this.Email, this.Birthday)}> 
                  <h4>Update User info:</h4>
                  <Form.Group>
                      <Form.Label>Name:</Form.Label>
                      <Form.Control>
                          type="text"
-                         defaultValue={username}
-                         onChange= {(e) => handleUpdate(e)}
+                         name="Username"
+                         onChange= {(e) => this.Username(e.target.value)}
                      </Form.Control>
                  </Form.Group>
                  <Form.Group>
                      <Form.Label>Password:</Form.Label>
                      <Form.Control>
                          type="password"
-                         defaultValue={password}
-                         onChange= {(e) => handleUpdate(e)}
+                         name="Password"
+                         onChange= {(e) => this.Password(e.target.value)}
                      </Form.Control>
                      </Form.Group>
                  <Form.Group>
                      <Form.Label>E-mail:</Form.Label>
                      <Form.Control>
                          type="email"
-                         defaultValue={email}
-                         onChange= {(e) => handleUpdate(e)}
+                         name="Email"
+                         onChange= {(e) => this.Email(e.target.value)}
                      </Form.Control>
                  </Form.Group>
                  <Form.Group>
                      <Form.Label>Birthday:</Form.Label>
                      <Form.Control>
                          type="date"
-                         defaultValue={birthday}
-                         onChange= {(e) => handleUpdate(e)}
-                         ref={birthdayInputRef}
-                 </Form.Control>
+                         name="Birthday"
+                         onChange= {(e) => this.Birthday(e.target.value)}
+                    </Form.Control>
                  </Form.Group>
                      <Form.Group>
-                     <Button variant="button" type="submit" >Submit</Button>
-                     {/* <Button variant="button" type="submit" onClick={() => this.onDeleteUser}>Delete</Button> */}
+                     <Button variant="button" className="edit-user" type="submit" onClick={() => this.editUser()}>Submit</Button>
+                     <Button variant="button" className="delete-user" type="submit" onClick={() => this.onDeleteUser()}>Delete</Button>
                  </Form.Group>
              </Form>
-
-        // <Form onSubmit={(e) => this.editUser(e, 
-        // this.username,
-        // this.password,
-        // this.email,
-        // this.birthday
-        // )}>
-        //     <h4>Update User info:</h4>
-        //     <Form.Group>
-        //         <Form.Label>Name:</Form.Label>
-        //         <Form.Control>
-        //             type="text"
-        //             defaultValue={username}
-        //             onChange= {(e) => this.setUsername(e.value.target)}
-        //         </Form.Control>
-        //     </Form.Group>
-        //     <Form.Group>
-        //         <Form.Label>Password:</Form.Label>
-        //         <Form.Control>
-        //             type="password"
-        //             defaultValue={password}
-        //             onChange= {(e) => this.setPassword(e.value.target)}
-        //         </Form.Control>
-        //         </Form.Group>
-        //     <Form.Group>
-        //         <Form.Label>E-mail:</Form.Label>
-        //         <Form.Control>
-        //             type="email"
-        //             defaultValue={email}
-        //             onChange= {(e) => this.setEmail(e.value.target)}
-        //         </Form.Control>
-        //     </Form.Group>
-        //     <Form.Group>
-        //         <Form.Label>Birthday:</Form.Label>
-        //         <Form.Control>
-        //             type="date"
-        //             defaultValue={birthday}
-        //             onChange= {(e) => this.setBirthday(e.value.target)}
-        //             ref={birthdayInputRef}
-        //     </Form.Control>
-        //     </Form.Group>
-        //         <Form.Group>
-        //         <Button variant="button" type="submit" onClick={() => this.editUser}>Update</Button>
-        //         <Button variant="button" type="submit" onClick={() => this.onDeleteUser}>Delete</Button>
-        //     </Form.Group>
-        // </Form>
     )
 }
 export default UpdateUser
