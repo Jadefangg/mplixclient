a3d29aad26196b7c7f55ab6bb8b6c13269b5dd4d
Hermann Rasch <hermann.rasch93@gmail.com>
2023-04-28 14:33:36 +0200

    ProfileView  corrections

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,50 +1,106 @@
-import React, {useEffect, useState} from "react"; 
-import Form from "react-bootstrap/Form";
-import { Col, ListGroupItem, Row}  from "react-bootstrap";
-import { Button, Card, Container } from "react-bootstrap";
-import { Link, Redirect } from "react-router-dom";
-import { PropTypes } from "prop-types";
-import axios from "axios";
+import  {React, useEffect, useState} from "react"; 
+import PropTypes from "prop-types";
+import { Col, Row }  from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 
-import {UserInfo} from "./user-info"; 
-import {UpdateUser} from "./update-user";
+import { FavoriteMovies } from "./favorite-movies";
+import { UserInfo }  from "./user-info";
+import { UpdateView } from "./update-user";
 import { DeleteUser } from "./delete-user";
-import {FavoriteMovies} from "./favorite-movies";
 
-function ProfileView ({ user, setUser, token, movies, removeFavMovie, onLoggedOut})  {
-console.log(user);
-console.log(user.Username);
-console.log(user.Email);
-return(
-        <Container>
-            <Row>
-              <Col>
-                <h2>Your Profile:</h2>
-              </Col>
-            </Row>
-            <Row> 
-                <Col xs={12} sm={4} md={6} lg={6} className="user-profile">
-                    <Card>
-                        <UserInfo username={user.Username} email={user.Email} birthday={user.Birthday.slice(0,10)}/>
-                    </Card>
+
+
+    export const ProfileView = ({user, /*setUser*/ movies, token, updateUser}) => {
+        
+        // const [myUser, setMyUser] = useState("");
+        // const [username, setUsername] = useState("");
+        // const [password, setPassword] = useState("");
+        // const [email, setEmail] = useState("");
+        // const [birthday, setBirthday] = useState("");
+
+        //get users 
+    useEffect(() => {
+        const token = window.localStorage.getItem("token");
+        fetch(`https://movies-couch-api.vercel.app/users/${user.Username}`,
+        {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` } })
+            .then((response) => {
+                if (response.ok) {
+                    console.log("User data retrieved")
+                } else {
+                    console.log("User data could not be retrieved.")
+                }
+                setMyUser(response.data);
+                console.log(response);
+        })
+            .catch((error) => {
+            console.error("An error ocurred" + error);
+            });
+    }, []);
+
+    
+    // remove-fav_Movies 
+     const removeFavMovie = async (movies) => {
+       const user = localStorage.getItem("user");
+       console.log(user);
+       const token = localStorage.getItem("token");
+       console.log(token);
+       fetch(`https://movies-couch-api.vercel.app/users/${user.Username}/movies/${movies._id}`,
+       {
+         method: "DELETE",
+        header: {Authorization: `Bearer ${token}`}}
+       ) 
+       .then((response) => {
+        if (response.ok) {
+            alert(`The Movie: ${movies._id} was removed from Favorite List`)
+            return response.json();
+          } else {
+            alert("Failed removing movie from Favorite list");
+            return false;
+          }
+        })
+       .catch(function (error) {
+           console.error(" An error ocurred" + error);
+           alert("Movie could not be removed.")
+       })
+    }
+
+
+    return (
+        <Container className="profile-view">
+                <Row className="d-flex justify-content-center p-4">
+                    <Col >
+                        <UserInfo user={user}/>
                     </Col>
-                    <Col xs={12} sm={4} md={6} lg={6} className="user-profile">
-                    {/* <DeleteUser onLoggedOut={onLoggedOut} token={token} user={user}/> */}
-                </Col>
-                <Col xs={12} sm={4} md={6} lg={6} className="user-profile">
-                {/* <UpdateUser set={setUser} token={token} user={user}/> */}
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                <h2>Favorite Movies List</h2>
-                {/* <FavoriteMovies movies={movies} removeFavMovie={removeFavMovie} user={user}/> */}
-                </Col>
-            </Row>
-        </Container>
+                    </Row> 
+                   <Row className="d-flex justify-content-center p-4">
+                        <Col >
+                            <UpdateView user={user} updateUser={updateUser}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <FavoriteMovies movies={movies} removeFavMovie={removeFavMovie} user={user}/>
+                        </Col>
+                    </Row>
+                    <Row className="d-flex justify-content-center p-4">
+                        <Col>
+                            <DeleteUser user={user}/>
+                        </Col>
+                    </Row>
+            </Container>
+        ); 
+};
 
-        );
+
+
+ProfileView.propTypes = {
+    user: PropTypes.object,
+    setUser: PropTypes.func, 
+    movies: PropTypes.array, 
+    token: PropTypes.string,
+    updateUser: PropTypes.func,
+    removeFavMovie: PropTypes.func,
 }
-
-export {ProfileView};
