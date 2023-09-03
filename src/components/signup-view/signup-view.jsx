99faac96819bee5baf304f1d846dd2d6d118a108
Hermann Rasch <hermann.rasch93@gmail.com>
2023-09-03 23:02:12 +0200

    Update signup-view.jsx

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -6,7 +6,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { error } from "console";
 
 export const SignupView = () => {
     const [username, setUsername] = useState("");
